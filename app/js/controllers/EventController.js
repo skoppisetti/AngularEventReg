c60f911d704e5125a64ac50d99e46bfcfafd9366
Suresh Koppisetti <suresh.koppisetti@gmail.com>
2015-10-08 19:46:22 -0400

    Changed success callback to $http's success and error promises

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,11 +3,13 @@
  */
 'use strict';
 
-eventsApp.controller('EventController', function EventController($scope, eventData) {
+eventsApp.controller('EventController', function EventController($scope, eventData, $log) {
     //$scope.event = eventData.event;
-    eventData.getEvent(function(event) {
-        $scope.event = event;
-    });
+    eventData.getEvent()
+        .success(function(event) {$scope.event = event;})
+        .error(function(data, status, headers, config) {
+                $log.warn(data, status, headers, config);
+            });
 
     $scope.upVoteSession = function(session) {
         session.upVoteCount++;
