9d6def65052845340b41cd8ffcfceb714801c107
Suresh Koppisetti <suresh.koppisetti@gmail.com>
2015-10-08 21:52:39 -0400

    Using the $promise object instead of binding directly to the event object.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -6,7 +6,10 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData) {
         $scope.sortorder = 'name';
-        $scope.event = eventData.getEvent();
+        eventData.getEvent()
+            .$promise
+            .then(function(event) { $scope.event = event; console.log(event);})
+            .catch(function(response) {console.log(response);});
 
         $scope.upVoteSession = function(session) {
         session.upVoteCount++;
